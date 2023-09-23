9badb68063fc8cbc64efc9a49a006ea732d361ec
Chris <cbonner.dev@outlook.com>
2023-09-23 20:37:09 +0100

    SEO Optimisations

--- scripts/index.js
+++ scripts/index.js
@@ -107,3 +107,20 @@ window.addEventListener("resize", function () {
         menuItems.style.display = "none"; // Close the menu
     }
 });
+
+(function (i, s, o, g, r, a, m) {
+  i["GoogleAnalyticsObject"] = r;
+  (i[r] =
+    i[r] ||
+    function () {
+      (i[r].q = i[r].q || []).push(arguments);
+    }),
+    (i[r].l = 1 * new Date());
+  (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m);
+})(window, document, "script", "//www.google-analytics.com/analytics.js", "ga");
+
+ga("create", "G-ELJEQYR4XS ", "auto");
+ga("send", "pageview");
